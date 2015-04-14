f2907b5b441c107af1d37088898991afa3dcefd2
Janaina Barbosa <nainabarbosa@users.noreply.github.com>
2015-04-14 12:46:04 -0300

    Update degree.py
    This function takes a member as an argument, and that first prints out all members who have 1 degree of separation from that member, then prints out all members that have 2 degrees of separation, then 3...  
    Didn't print the same member twice.

--- degree.py
+++ degree.py
@@ -1,6 +1,3 @@
-This function takes a member as an argument, and that first prints out all members who have 1 degree of separation from that member, then prints out all members that have 2 degrees of separation, then 3...  
-Didn't print the same member twice.
-
 petitions = {'a': [1,3,4,6], 'b': [2,3,4,6,7], 'c': [1,2,3,4,5,6]}
  
 users = {1: ['a', 'b'], 2: ['b', 'c'], 3: ['a', 'b', 'c'], 4: ['a', 'b', 'c'], 5: ['c'], 6: ['c', 'b', 'c']}
