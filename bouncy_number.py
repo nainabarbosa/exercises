0719af91c8a05d39f91da5f8c48233f541b53380
Janaina Barbosa <nainabarbosa@users.noreply.github.com>
2019-05-04 15:45:03 -0300

    Update bouncy_number.py

--- bouncy_number.py
+++ bouncy_number.py
@@ -1,1 +1,26 @@
+# If incr = True and decr = True, then is a bouncy number. 
+def is_bouncy(num):
+    incr, decr = False, False
+    right_digit = num % 10
+    num = num // 10
 
+    while num > 0:
+        left_digit = num % 10
+        if left_digit < right_digit:
+            incr = True
+        elif left_digit > right_digit:
+            decr = True
+        right_digit = left_digit
+        num = num // 10
+        if incr and decr:
+            return True
+    return False
+
+# Iterate through numbers until the bouncy count is 99%
+count = 0
+i = 99
+while count < 0.99 * i:
+    i = i + 1
+    if is_bouncy(i):
+        count = count + 1
+print(i)
