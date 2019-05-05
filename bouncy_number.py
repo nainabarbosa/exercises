eaafcf280564e4c09c4e0b0f419235a5ab65147b
Janaina Barbosa <nainabarbosa@users.noreply.github.com>
2019-05-05 09:43:16 -0300

    Update bouncy_number.py

--- bouncy_number.py
+++ bouncy_number.py
@@ -6,16 +6,17 @@ def is_bouncy(num):
             increasing = True
         elif num_str[i+1] < num_str[i]:
             decreasing = True
-        if increasing and decreasing: 
+        if increasing and decreasing:
             return True
     return False
 
-count = 0
-percentage = 0.0
-n = 99
-while int(percentage) != 99:    
-    n = n + 1
-    if is_bouncy(n):
-        count = count + 1
-    percentage = (count*100.0)/n
-print('{0} is the least number for which the proportion of bouncy numbers is exactly 99%.'.format(n))
+def find_least():
+    count = 0
+    n = 99
+    while count < 0.99 * n:
+        n = n + 1
+        if is_bouncy(n):
+            count = count + 1
+    print('{0} is the least number for which the proportion of bouncy numbers is exactly 99%.'.format(n))
+
+find_least()
